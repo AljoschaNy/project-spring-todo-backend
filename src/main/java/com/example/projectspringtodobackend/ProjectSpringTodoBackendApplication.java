a4cf6b58dbbf988b886dac43cfcb33df93461e40
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-20 14:50:04 +0200

    Initial commit

---
+++ src/main/java/com/example/projectspringtodobackend/ProjectSpringTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectspringtodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringTodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectspringtodobackend/ProjectSpringTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectspringtodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

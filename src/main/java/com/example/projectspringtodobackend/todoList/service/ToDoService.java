6c3b8bf00db182d85006c2c433b0553f6dbdf8b3
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-20 23:20:22 +0200

    Add unit test for addToDo method and implement method in ToDoService

--- src/main/java/com/example/projectspringtodobackend/todoList/service/ToDoService.java
+++ src/main/java/com/example/projectspringtodobackend/todoList/service/ToDoService.java
@@ -1,5 +1,6 @@
 package com.example.projectspringtodobackend.todoList.service;
 
+import com.example.projectspringtodobackend.todoList.model.ToDo;
 import com.example.projectspringtodobackend.todoList.repository.ToDoRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ToDoService {
     private final ToDoRepo toDoRepo;
+
+
+    public ToDo addToDo(ToDo toDo) {
+        return toDoRepo.save(toDo);
+    }
 }
